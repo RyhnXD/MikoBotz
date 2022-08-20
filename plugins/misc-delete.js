0120e49bdb567678c505bef26eeb41afdf543c44
雨 ʀᴀʏʜᴀɴ サンダー <102699105+RyhnXD@users.noreply.github.com>
2022-08-20 22:40:33 +0700

    Delete plugins directory

--- plugins/misc-delete.js
+++
@@ -1,7 +0,0 @@
-function handler(m) {
-    if (m.quoted && m.quoted.fromMe) this.sendMessage(m.chat, { delete: m.quoted.vM.key })
-}
-handler.alias = ['del', 'delete']
-handler.command = /^del(ete)?$/i
-
-export default handler
